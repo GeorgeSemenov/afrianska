e4a05b553eeddcc6fb2db1a49c005768feaf74de
GeorgeSemenov <zhora.sem@yandex.ru>
2021-02-19 16:15:58 +0300

    start card

--- source/blocks/main-screen/main-screen.js
+++ source/blocks/main-screen/main-screen.js
@@ -1,2 +1,7 @@
 $(document).ready(function(){
+  $(".main-screen__link").click(function(){
+    $("body,html").stop(true,false).animate({
+      scrollTop:$("#" + $(this).data('screen-to-go')).offset().top
+    },1000);
+  })
 })
