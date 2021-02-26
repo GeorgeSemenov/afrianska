d5fb68d848febf746790a79f466cd48eb84dcf65
GeorgeSemenov <zhora.sem@yandex.ru>
2021-02-26 16:19:50 +0300

    pagination

--- source/blocks/cards-screen/cards-screen.js
+++ source/blocks/cards-screen/cards-screen.js
@@ -1,2 +1,38 @@
+import Swiper from '../../../node_modules/swiper/swiper-bundle.js';
 $(document).ready(function(){
+
+  var breakpoint= window.matchMedia('(min-width:767px)');
+  var swiper; // объявляю переменную, чтобы она была видна во всех функциях
+
+  var breakpointChecker = function(){
+    if (breakpoint.matches === true) {
+      if (swiper !== undefined) {
+        return swiper.destroy();
+      }
+    } else if (breakpoint.matches === false) {
+      return enableSwiper();
+    }
+  };
+
+  var enableSwiper = function () {
+      swiper = new Swiper('.cards-screen', {
+      slidesPerView: 1,
+      spaceBetween: 15,
+      pagination: {
+        el: '.cards-screen__pagination',
+      },
+      autoplay: {
+        delay: 3500,
+        disableOnInteraction: false,
+      },
+      breakpoints:{
+        510:{
+          slidesPerView: 2,
+          spaceBetween: 15,
+        },
+      }
+    });
+  };
+  breakpoint.addListener(breakpointChecker);
+  breakpointChecker();
 })
--- source/pages/index/index.js
+++ source/pages/index/index.js
@@ -1,3 +1,4 @@
 import './index.scss';
 import '../../JS-components/libs.js';
 import '../../blocks/header/header.js';
+import '../../blocks/cards-screen/cards-screen.js';
