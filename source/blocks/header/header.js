fb540a83528ab4867f6028193ae5e563ec948b74
GeorgeSemenov <zhora.sem@yandex.ru>
2021-03-15 16:05:46 +0300

    before lesson

--- source/blocks/header/header.js
+++ source/blocks/header/header.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
   $('.header__sandwich-ico-container').click(function(){
     $('.header').toggleClass('header_expanded');
-  })
+  });
+  $(window).scroll(function () {//При скроле более чем на 100 px хедер получает модификатор, который изменяет его высоту(только для десктопа и плоншетов)
+    if (($(window).scrollTop()>=(100)) && ($(window).width()>=767) ){
+      $(".header").addClass('header_theme_small');
+    }
+    else{
+      $(".header").removeClass('header_theme_small');
+    }
+  });
 })
