c24caf7e607b43046a88b7d7910a07f495e655f4
GeorgeSemenov <zhora.sem@yandex.ru>
2021-03-16 16:09:53 +0300

    semantic work is a great wheather today

--- source/blocks/header/header.js
+++ source/blocks/header/header.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     $('.header').toggleClass('header_expanded');
   });
   $(window).scroll(function () {//При скроле более чем на 100 px хедер получает модификатор, который изменяет его высоту(только для десктопа и плоншетов)
-    if (($(window).scrollTop()>=(100)) && ($(window).width()>=767) ){
+    if (($(window).scrollTop()>=(100)) ){
       $(".header").addClass('header_theme_small');
     }
     else{
