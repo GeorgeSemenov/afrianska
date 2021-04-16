b95cdc7ee0d1f7e1bfc8ec86f6d36687501665b6
GeorgeSemenov <zhora.sem@yandex.ru>
2021-04-16 16:03:39 +0300

    site almost donw

--- source/blocks/header/header.js
+++ source/blocks/header/header.js
@@ -2,12 +2,15 @@ $(document).ready(function(){
   $('.header__sandwich-ico-container').click(function(){
     $('.header').toggleClass('header_expanded');
   });
+  if ( $('.header').offset().top >=100 ){//При загрузке страници, если хедер отстаёт от начала документа более чем на 100 пыкселей то будет выполнятся следующие действия
+    $(".header").addClass('header_theme_small header_theme_white');
+  }
   $(window).scroll(function () {//При скроле более чем на 100 px хедер получает модификатор, который изменяет его высоту(только для десктопа и плоншетов)
     if (($(window).scrollTop()>=(100)) ){
-      $(".header").addClass('header_theme_small');
+      $(".header").addClass('header_theme_small header_theme_white');
     }
     else{
-      $(".header").removeClass('header_theme_small');
+      $(".header").removeClass('header_theme_small header_theme_white');
     }
   });
 })
